05c2f565764f5a9459acd247d634a550e751ea98
Karolina <kar.pawlowska@student.uj.edu.pl>
2023-09-07 21:03:47 +0200

    kategorie i uzytkownik

---
+++ app/src/Service/CategoryService.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Category service.
+ */
+
+namespace App\Service;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+/**
+ * Class CategoryService.
+ */
+class CategoryService implements CategoryServiceInterface
+{
+    /**
+     * Category repository.
+     */
+    private CategoryRepository $categoryRepository;
+
+    /**
+     * Paginator.
+     */
+    private PaginatorInterface $paginator;
+
+    /**
+     * CategoryService constructor.
+     *
+     * @param CategoryRepository $categoryRepository Category repository
+     * @param PaginatorInterface $paginator          Paginator
+     */
+    public function __construct(CategoryRepository $categoryRepository, PaginatorInterface $paginator)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * Get paginated list.
+     *
+     * @param int $page Page number
+     *
+     * @return PaginationInterface<string, mixed> Paginated list
+     */
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        return $this->paginator->paginate(
+            $this->categoryRepository->queryAll(),
+            $page,
+            CategoryRepository::PAGINATOR_ITEMS_PER_PAGE
+        );
+    }
+
+    /**
+     * Save entity.
+     *
+     * @param Category $category Category entity
+     */
+    public function save(Category $category): void
+    {
+        $this->categoryRepository->save($category);
+    }
+
+    /**
+     * Delete entity.
+     *
+     * @param Category $category Category entity
+     */
+    public function delete(Category $category): void
+    {
+        $this->categoryRepository->delete($category);
+    }
+
+    /**
+     * Find one by id.
+     *
+     * @param int $id Category id
+     *
+     * @return Category|null Category entity
+     *
+     * @throws NonUniqueResultException
+     */
+    public function findOneById(int $id): ?Category
+    {
+        return $this->categoryRepository->findOneById($id);
+    }
+}
